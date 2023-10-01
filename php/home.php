ce69fb05c01936e4e971bd2233b2444d2c02c5eb
Juan Miguel Acosta <acostaojuanmi@gmail.com>
2023-10-01 21:38:23 +0200

    feat: logo sign out.

--- php/home.php
+++ php/home.php
@@ -1,17 +1,17 @@
 <?php
-    session_start();
-    if (!isset($_SESSION['usuario'])) {
-        echo '
+session_start();
+if (!isset($_SESSION['usuario'])) {
+    echo '
             <script>
                 alert("You must login first");      
                 window.location = "../index.php";      
             </script>
         ';
-        session_destroy();
-        die();
-    }else{
-        $usuario = $_SESSION['usuario'];
-    }
+    session_destroy();
+    die();
+} else {
+    $usuario = $_SESSION['usuario'];
+}
 ?>
 
 <!DOCTYPE html>
@@ -60,25 +60,32 @@
                         </button>
                     </a>
                 </li>
-                <!-- poner icono sign out --> 
-                <li><a href="../php/close_session.php">Close session</a></li>
+                <!-- poner icono sign out -->
+                <li><a href="../php/close_session.php"><svg xmlns="http://www.w3.org/2000/svg" height="1em"
+                            viewBox="0 0 576 512">
+                            <style>
+                                svg {
+                                    fill: #ffffff
+                                }
+                            </style>
+                            <path
+                                d="M320 32c0-9.9-4.5-19.2-12.3-25.2S289.8-1.4 280.2 1l-179.9 45C79 51.3 64 70.5 64 92.5V448H32c-17.7 0-32 14.3-32 32s14.3 32 32 32H96 288h32V480 32zM256 256c0 17.7-10.7 32-24 32s-24-14.3-24-32s10.7-32 24-32s24 14.3 24 32zm96-128h96V480c0 17.7 14.3 32 32 32h64c17.7 0 32-14.3 32-32s-14.3-32-32-32H512V128c0-35.3-28.7-64-64-64H352v64z" />
+                        </svg>
+                    </a></li>
             </ul>
         </nav>
-
-
-
     </header>
 
     <div class="container">
 
         <div id="prueba">
             <h1 style="color:white;">🐧 Welcome,
-                <?php echo "@$usuario" ?> 
+                <?php echo "@$usuario" ?>
             </h1>
         </div>
 
     </div>
-  </script>
+    </script>
 
 </body>
 
